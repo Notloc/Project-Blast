100bdc7cae81a6b3b5a9d90ceb6762df52c20708
Notloc <ColtonJamesCampbell@gmail.com>
2020-05-06 17:33:16 -0300

    Further work on terrain gen, got terrain generation working properly with offset and configured texture generation.

---
+++ Assets/Scripts/WorldGen/TerrainGenerator.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class and related classes are based on this tutorial series
+// https://www.youtube.com/playlist?list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3
+public class TerrainGenerator : MonoBehaviour
+{
+    enum DisplayMode
+    {
+        HEIGHT_MAP,
+        COLOR
+    }
+
+    public bool AutoUpdate = false;
+
+    [Header("Noise Generation Settings")]
+    [SerializeField] Vector2Int terrainResolution = Vector2Int.one;
+    [SerializeField] float noiseScale = 1f;
+    [Range(0, 15)]
+    [SerializeField] int octaves = 3;
+    [Range(0f,1f)]
+    [SerializeField] float persistance = 1f;
+    [SerializeField] float lacunarity = 1f;    
+    [SerializeField] Vector2 noiseOffset = Vector2.zero;
+    [SerializeField] int noiseSeed = 9999;
+    [SerializeField] TerrainNoise.NormalizeMode normalizeMode = TerrainNoise.NormalizeMode.Global;
+
+    [Header("Texture Settings")]
+    [SerializeField] new Renderer renderer = null;
+    [SerializeField] DisplayMode displayMode = DisplayMode.COLOR;
+    [SerializeField] List<TerrainRegion> regions = null;
+    
+    [Header("Mesh Settings")]
+    [SerializeField] float heightScale = 100f;
+    [SerializeField] AnimationCurve heightCurve = null;
+    [SerializeField] MeshFilter meshFilter = null;
+    [SerializeField] MeshCollider meshCollider = null;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        int width = terrainResolution.x;
+        int height = terrainResolution.y;
+
+        float[,] heightMap = TerrainNoise.GenerateNoiseMap(width, height, noiseSeed, noiseScale, octaves, persistance, lacunarity, noiseOffset, normalizeMode);
+
+        Color[] colorMap = new Color[width * height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++)
+            {
+                int index = width * y + x;
+                colorMap[index] = GetColor(heightMap[x,y]);
+            }
+        }
+
+        Texture2D tex;
+        if (displayMode == DisplayMode.COLOR)
+            tex = TextureGenerator.TextureFromColorMap(colorMap, width, height);
+        else
+            tex = TextureGenerator.TextureFromHeightMap(heightMap);
+
+        renderer.sharedMaterial.SetTexture("_MainTex", tex);
+
+        var mesh = MeshGenerator.GenerateMesh(heightMap, heightScale, heightCurve);
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+    }
+
+    private Color32 GetColor(float height)
+    {
+        Color col = Color.black;
+
+        for (int i = 0; i < regions.Count; i++)
+            if (regions[i].height <= height)
+                col = regions[i].color;
+
+        return col;
+    }
+
+    private void OnValidate()
+    {
+        if (terrainResolution.x < 1)
+            terrainResolution.x = 1;
+        if (terrainResolution.y < 1)
+            terrainResolution.y = 1;
+
+        if (octaves < 0)
+            octaves = 0;
+
+        if (lacunarity < 1)
+            lacunarity = 1;
+    }
+}
+[System.Serializable]
+public struct TerrainRegion
+{
+    public string name;
+    public float height;
+    public Color color;
+}
---
+++ Assets/Scripts/WorldGen/TerrainNoise.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TerrainNoise
+{
+    public enum NormalizeMode { Local, Global }
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset, NormalizeMode normalizeMode)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        float maxPossibleHeight = 0f;
+        float amplitude = 1;
+
+        System.Random rng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            octaveOffsets[i] = new Vector2(
+                rng.Next(-100000, 100000) + offset.x,
+                rng.Next(-100000, 100000) + offset.y
+            );
+
+            maxPossibleHeight += amplitude;
+            amplitude = amplitude * persistance;
+        }
+
+        float maxLocalNoiseVal = float.MinValue;
+        float minLocalNoiseVal = float.MaxValue;
+
+        float halfW = mapWidth / 2f;
+        float halfH = mapHeight / 2f;
+
+        for (float y = 0; y < mapHeight; y++)
+        {
+            for (float x = 0; x < mapWidth; x++)
+            {
+
+                amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float xCoord = (x - halfW + octaveOffsets[i].x) / scale * frequency;
+                    float yCoord = (y - halfH + octaveOffsets[i].y) / scale * frequency;
+
+                    float val = Mathf.PerlinNoise(xCoord, yCoord) * 2f - 1f;
+                    noiseHeight += val * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+
+                    if (val > maxLocalNoiseVal)
+                        maxLocalNoiseVal = val;
+                    if (val < minLocalNoiseVal)
+                        minLocalNoiseVal = val;
+                }
+
+                noiseMap[(int)x, (int)y] = noiseHeight;
+            }
+        }
+
+
+
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                if (normalizeMode == NormalizeMode.Local) {
+                    noiseMap[x, y] = Mathf.InverseLerp(minLocalNoiseVal, maxLocalNoiseVal, noiseMap[x, y]);
+                }
+                else {
+                    noiseMap[x, y] = Mathf.Clamp((noiseMap[x, y] + 1f) / maxPossibleHeight, 0f, float.MaxValue);
+                }
+            }
+        }
+        return noiseMap;
+    }
+}
