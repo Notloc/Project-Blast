1ac79347e6fe5426f8f497f5ec7b2e23dd6b67b3
Notloc <ColtonJamesCampbell@gmail.com>
2020-08-10 23:24:26 -0300

    Began implementing Equipment

---
+++ Assets/Scripts/Equipment/EquipmentBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class EquipmentBase : ItemBase
+{
+    public abstract EquipmentType EquipmentType { get; }
+}
---
+++ Assets/Scripts/Equipment/Weapons/GunBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/New Gun")]
+public class GunBase : EquipmentBase
+{
+    public override EquipmentType EquipmentType => EquipmentType.GUN;
+
+
+    [SerializeField] Projectile projectile;
+    public Projectile Projectile { get { return projectile; } }
+
+}
