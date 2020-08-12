a22eda4194b982289064f06b47d6aee2956b2b3d
Notloc <ColtonJamesCampbell@gmail.com>
2020-08-11 23:37:14 -0300

    Added bullet trails.

--- Assets/Scripts/Equipment/Weapons/Projectile.cs
+++ Assets/Scripts/Equipment/Weapons/Projectile.cs
@@ -6,16 +6,33 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] new Collider collider = null;
     [SerializeField] new Rigidbody rigidbody = null;
-
+    [SerializeField] TrailRenderer trailRenderer = null;
+    
     private float damage;
+    private bool live;
 
     public void Init(float damage, float speed, Collider[] colliders)
     {
+        this.damage = damage;
+        
         // Impossible to shoot self
         foreach (Collider c in colliders)
             Physics.IgnoreCollision(collider, c);
 
-        this.damage = damage;
+        trailRenderer.emitting = true;
         rigidbody.velocity = transform.forward * speed;
+        live = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IDamagable damagable = collision.collider.GetComponent<IDamagable>();
+        if (!damagable.IsNull())
+        {
+            damagable.Damage(damage);
+        }
+
+        trailRenderer.emitting = false;
+        live = false;
     }
 }
